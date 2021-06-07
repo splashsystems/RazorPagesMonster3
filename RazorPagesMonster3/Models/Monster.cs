abdaf87e43748addbe92b1f7d68b56c20488414c
Scot <bscot.hagen@gmail.com>
2021-06-07 09:29:37 -0700

    Added Model properties

---
+++ RazorPagesMonster3/Models/Monster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMonster3.Models
+{
+    public class Monster
+    {
+        public int ID { get; set; }
+        public string MonsterName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
